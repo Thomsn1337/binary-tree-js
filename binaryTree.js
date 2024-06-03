732f7444e9ab976afd6a544ff361720306b44035
Thomas Steifler <steiflerthomas@gmail.com>
2024-06-03 16:57:07 +0200

    add basic node class

---
+++ binaryTree.js
@@ -0,0 +1,19 @@
+class TreeNode {
+    /**
+     * @constructor
+     * @param {number} data
+     */
+    constructor(data) {
+        this.data = data;
+
+        /**
+         * @type {TreeNode | null}
+         */
+        this.left = null;
+
+        /**
+         * @type {TreeNode | null}
+         */
+        this.right = null;
+    }
+}
