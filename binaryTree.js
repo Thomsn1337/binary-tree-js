436d0ed4b0a207a1a3e171d2a3dfcde53ce674db
Thomas Steifler <steiflerthomas@gmail.com>
2024-06-03 16:58:28 +0200

    add tree class and tree building method

--- binaryTree.js
+++ binaryTree.js
@@ -1,3 +1,4 @@
+import mergeSort from "./mergeSort.js";
 class TreeNode {
     /**
      * @constructor
@@ -17,3 +18,36 @@ class TreeNode {
         this.right = null;
     }
 }
+
+class Tree {
+    /**
+     * @constructor
+     * @param {number[]} arr
+     */
+    constructor(arr) {
+        const uniqueArr = mergeSort([...new Set(arr)]);
+
+        this.root = this.buildTree(uniqueArr);
+    }
+
+    /**
+     * @param {number[]} arr
+     * @param {number} [start=0]
+     * @param {number} [end=arr.length - 1]
+     *
+     * @returns {TreeNode | null}
+     */
+    buildTree(arr, start = 0, end = arr.length - 1) {
+        if (start > end) return null;
+
+        const mid = Math.floor((start + end) / 2);
+
+        const node = new TreeNode(arr[mid]);
+        node.left = this.buildTree(arr, start, mid - 1);
+        node.right = this.buildTree(arr, mid + 1, end);
+
+        return node;
+    }
+}
+
+export default Tree;
